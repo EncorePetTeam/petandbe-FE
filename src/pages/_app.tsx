1e6ef047df8889c1fe9ceb3481cbea0dd114e662
Jinwoo Choi <55120608+Minus960703@users.noreply.github.com>
2022-12-01 12:56:27 +0900

    [Feat] 카카오맵 API 설정 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import '../styles/index.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/templates/Layout/Layout';
 
+declare global {
+  interface Window {
+    kakao :any;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>

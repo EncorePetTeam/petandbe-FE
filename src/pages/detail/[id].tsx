c3c2d58470f0fc0ef7069e1a27f85bd34c806556
Jinwoo Choi <cgzoo9607@gmail.com>
2022-12-03 10:00:34 +0900

    [Fix] 페이지 구조 수정

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -1,16 +1,75 @@
 import { useRouter } from 'next/router';
 import React from 'react'
+import { DetailImage } from '../../components/atoms/Detail/DetailImage';
+import { DetailReserve } from '../../components/atoms/Detail/DetailReserve';
+import { DetailReview } from '../../components/atoms/Detail/DetailReview';
+import { HostInfo } from '../../components/atoms/HostInfo';
+import { Location } from '../../components/atoms/Location';
+import { RequiredRules } from '../../components/atoms/RequiredRules';
 import { SeoHead } from '../../components/atoms/SeoHead';
+import { DetailHostInfo } from '../../components/molecules/Detail/DetailHostInfo';
+import { DetailMenu } from '../../components/molecules/Detail/DetailMenu';
+import { FlexBox } from '../../components/molecules/FlexBox';
+import { LeftFlexBox } from '../../components/molecules/FlexBox/LeftFlexBox';
+import { RightFlexBox } from '../../components/molecules/FlexBox/RightFlexBox';
 import { Section } from '../../components/organisms/Section';
 
-type Props = {}
+const detailReview = [
+  {
+    id: 1,
+    userName: 'Kyla',
+    regDate: '2022년 11월',
+    content: '10/10 would recommend staying here',
+  },
+  {
+    id: 2,
+    userName: 'Kyla',
+    regDate: '2022년 11월',
+    content: '10/10 would recommend staying here',
+  },
+  {
+    id: 3,
+    userName: 'Kyla',
+    regDate: '2022년 11월',
+    content: '10/10 would recommend staying here',
+  },
+  {
+    id: 4,
+    userName: 'Kyla',
+    regDate: '2022년 11월',
+    content: '10/10 would recommend staying here',
+  },
+  {
+    id: 5,
+    userName: 'Kyla',
+    regDate: '2022년 11월',
+    content: '10/10 would recommend staying here',
+  },
+]
 
-const DetailPage = (props: Props) => {
+const DetailPage = () => {
   const productsName = 'Ex'
   return (
     <div className='detail__content'>
       <SeoHead title={`펫앤비 | ${productsName}`} />
-      <Section />
+      <DetailMenu />
+      <DetailImage />
+      <FlexBox>
+        <LeftFlexBox width={'60%'}>
+          <DetailHostInfo />
+        </LeftFlexBox>
+        <RightFlexBox width={'40%'}>
+          <DetailReserve />
+        </RightFlexBox>
+      </FlexBox>
+      <hr />
+      <DetailReview detailReview={detailReview}/>
+      <hr />
+      <Location />
+      <hr />
+      <HostInfo />
+      <hr />
+      <RequiredRules />
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,218 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import { SeoHead } from '../components/atoms/SeoHead'
+import { FlexBox } from '../components/molecules/FlexBox'
+import { HomeContent } from '../components/molecules/HomeContent'
 import { HomeSection } from '../components/organisms/HomeSection'
 
+const main = [
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 1,
+    like: true,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 2,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 3,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 4,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 5,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 6,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 7,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 8,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 9,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 10,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 11,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 12,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 13,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 14,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 15,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 16,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 17,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 18,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 19,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 20,
+    like: false,
+  },
+  {
+    location: '중구, 한국',
+    distance: 50,
+    date: '11월 14일 ~ 19일',
+    price: 180200,
+    imgUrl: '',
+    id: 21,
+    like: false,
+  },
+]
+
 export default function Home() {
+  const [contents, setContents] = useState(main);
+  const isActiveBookmark = (id :number) => {    
+    setContents(contents.map(
+      (content) => content.id === id 
+      ? {...content, like: !content.like}
+      : content
+    ))        
+  }
   return (
     <div>
       <SeoHead title={'펫앤비 | Home'} />
-      <HomeSection />
+      <FlexBox>
+        {contents.map((current)=><HomeContent content={current} key={current.id} isActiveBookmark={isActiveBookmark}/>)}
+      </FlexBox>
     </div>
   )
 }
